d75803f0e7a2f063b9475f94681ee678efa02001
Liomar <liomarhora@gmail.com>
2024-05-06 18:50:42 -0300

    add class

---
+++ src/treeviewderived.cpp
@@ -0,0 +1,5 @@
+#include "treeviewderided.h"
+
+
+
+
---
+++ src/treeviewderived.h
@@ -0,0 +1,13 @@
+#ifndef TREEVIEW_DERIVED_H
+#define TREEVIEW_DERIVED_H
+
+
+#include <gtkmm.h>
+
+class TreeviewDerived : public Gtk::TreeView
+{
+	
+};
+
+
+#endif /* TREEVIEW_DERIVED_H */
